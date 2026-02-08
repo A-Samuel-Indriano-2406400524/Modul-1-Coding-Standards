e9e2f6c99d6d4cde1f42baac632a919ada8a58d2
samindriano <samuelindriano@gmail.com>
2026-02-08 22:54:03 +0700

    Implement findProductById and updateProduct

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
@@ -18,5 +19,25 @@ public class ProductRepository {
 
     public Iterator<Product> findAll(){
         return productData.iterator();
+    }
+    
+    public Product findProductById(String id){
+        for (Product product : productData){
+            if (product.getProductId().equals(id)){
+                return product;
+            }
+        }
+        return null;
+    }
+    
+    public Product updateProduct(Product product){
+        for (int i = 0; i < productData.size(); i++){
+            Product p = productData.get(i);
+            if (p.getProductId().equals(product.getProductId())){
+                productData.set(i, product);
+                return product;
+            }
+        }
+        return null;
     }
 }
